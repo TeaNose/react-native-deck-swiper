afabb30987d961193ada158d5cd2507a37add3fc
Agustinus Wikrama <agustinuswikrama@gmail.com>
2018-04-17 11:36:33 +0700

    Update styles.js

--- styles.js
+++ styles.js
@@ -6,12 +6,7 @@ const styles = StyleSheet.create({
     position: 'absolute'
   },
   container: {
-    alignItems: 'stretch',
-    position: 'absolute',
-    top: 0,
-    left: 0,
-    right: 0,
-    bottom: 0
+    flex: 1,
   },
   childrenViewStyle: {
     position: 'absolute',
